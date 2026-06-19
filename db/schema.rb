0935c5bd57275eba753db8e8ec3d1216309343ad
kazkaz9717 <kazkaz9717@gmail.com>
2026-06-19 16:16:47 +0900

    不要になったcategoryカラム、バックアップしたJSファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_18_111242) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_19_070705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,7 +83,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_18_111242) do
     t.integer "monthly_fee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "category"
     t.string "group_name"
   end
 
